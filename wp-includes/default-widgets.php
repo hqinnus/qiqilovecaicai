ef0807cb5a47b87ae50af8880c4e009d35a1952f
Huang Qi <huangqiyx@outlook.com>
2013-12-31 11:35:02 +0800

    remove date info from home page posts

--- wp-includes/default-widgets.php
+++ wp-includes/default-widgets.php
@@ -576,7 +576,7 @@ class WP_Widget_Recent_Posts extends WP_Widget {
 			<li>
 				<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
 			<?php if ( $show_date ) : ?>
-				<span class="post-date"><?php echo get_the_date(); ?></span>
+				<span class="post-date"></span>
 			<?php endif; ?>
 			</li>
 		<?php endwhile; ?>
