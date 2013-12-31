dc2621e124f07abae92f7202acb4c4d7e84f8cfc
Huang Qi <huangqiyx@outlook.com>
2013-12-31 11:42:07 +0800

    update home page tag widget to display name

--- wp-content/themes/buttercream/inc/template-tags.php
+++ wp-content/themes/buttercream/inc/template-tags.php
@@ -124,8 +124,8 @@ function buttercream_posted_on() {
 	echo '<a href="' . get_permalink() . '" rel="bookmark" title="' . __( 'Permanent Link to' , 'buttercream' ) . get_the_title() . '">';
 
 	if( get_post_type() !== 'page' ) {
-		echo '<span class="post-month">' .  '</span> ';
-		echo '<span class="post-day">' . '</span> ';
+		echo '<span class="post-month">' . 'QiQi' . '</span> ';
+		echo '<span class="post-day">' . 'CaiCai' .'</span> ';
 	} else {
 		echo '<span class="page-post-month">Page</span>';
 	}
